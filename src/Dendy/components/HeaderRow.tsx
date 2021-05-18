fb07a568399ee8bd03bc7cbaac0299ef25a77e21
Fisayo <48415629+fisayowatti@users.noreply.github.com>
2021-05-18 19:04:09 +0100

    use accurate reference to the profile picture

--- src/Dendy/components/HeaderRow.tsx
+++ src/Dendy/components/HeaderRow.tsx
@@ -18,7 +18,10 @@ export default function HeaderRow({ onMenuClick }: HeaderRowProps) {
       <SharedElement id={`item.profileImage`}>
         <Image
           style={styles.image}
-          source={{ uri: "https://source.unsplash.com/random/600x600" }}
+          source={{
+            uri:
+              "https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+          }}
           height={56}
           width={56}
         />
